781e03f16d0861b05c7c2838203c410ac4fab93c
Igor <igorponticelli8@gmail.com>
2023-08-15 11:09:04 -0300

    alest_2

--- alest.java
+++ alest.java
@@ -1,5 +1,27 @@
+import java.util.*;
 public class alest{
-    public static void main(){
-        System.out.println("Falaaaaa");
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor de n1");
+        int n = sc.nextInt(); 
+
+        funcao1(n);
+        funcao2(n);
+    }
+    public static void funcao1(int n){
+        double antes = System.currentTimeMillis();
+        System.out.println(2*(n*n) + 5*n);
+        double depois = System.currentTimeMillis();
+        System.out.println("Tempo de execução: " + (depois-antes));
+
+    }
+
+    public static void funcao2(int n){
+        double antes = System.currentTimeMillis();
+        System.out.println(500*n + 4000);
+        double depois = System.currentTimeMillis();
+        System.out.println("Tempo de execução: " + (depois-antes));
+
     }
 }
