4d2e7e04d011597dd7197a9158d135706bb91d36
Igor <igorponticelli8@gmail.com>
2023-08-22 10:33:38 -0300

    alest__6

---
+++ alest_2.java
@@ -0,0 +1,9 @@
+public class alest_2 {
+    public static void main(String [] args){
+        int soma = 0;
+        for(int i =1;i<=10;i++){
+            soma+= i;
+        }
+            System.out.println(soma);
+    }
+}
