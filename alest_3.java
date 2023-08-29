e0955a10963340b99ad92b761b886274c706f5d0
Igor <igorponticelli8@gmail.com>
2023-08-29 11:17:40 -0300

    alest__7

---
+++ alest_3.java
@@ -0,0 +1,24 @@
+public class alest_3 {
+    public static void main(String[]args){
+       int cont = 0;
+        recursao1();
+        recursao2(cont);
+        
+    }
+    
+    public static void recursao1(){
+        for(int i =0;i<5;i++){
+            System.out.println("Recursao 1 Faaaalaaaaaaa");
+        }
+    } 
+
+    public static void recursao2(int cont){
+        System.out.println("Recursao 2 Faaaalaaaa");
+        cont++;
+        if(cont <5){
+            recursao2(cont);
+        }
+
+    }
+   
+}
