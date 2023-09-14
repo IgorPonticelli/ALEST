0dac6f4e2133d732840f507667380ef9390ebbb0
Igor <igorponticelli8@gmail.com>
2023-09-14 11:12:17 -0300

    Aula14.09

---
+++ Aula14/ListArrayOfInteger.java
@@ -0,0 +1,115 @@
+package Aula14;
+/**
+ * Classe que implementa uma lista linear usando arranjo.
+ * @author Isabel H. Manssour
+ */
+
+public class ListArrayOfInteger{
+
+    // Atributos
+    private static final int INITIAL_SIZE = 10;
+    private Integer[] data;
+    private int count;
+
+    /**
+     * Construtor da lista.
+     */
+    public ListArrayOfInteger() {
+        this(INITIAL_SIZE);
+    }
+
+    /**
+     * Construtor da lista.
+     * @param tam tamanho inicial a ser alocado para data[]
+     */
+    public ListArrayOfInteger(int tam) {
+        if (tam <= 0) {
+            tam = INITIAL_SIZE;
+        }
+        data = new Integer[tam];
+        count = 0;
+    }
+
+    /**
+     * Esvazia a lista.
+     */
+    public void clear() { 
+        data = new Integer[INITIAL_SIZE];
+        count = 0;
+    }
+
+    /**
+     * Retorna true se a lista nao contem elementos.
+     * @return true se a lista nao contem elementos
+     */
+    public boolean isEmpty() { 
+        return (count == 0);
+    }
+
+    /**
+     * Retorna o numero de elementos armazenados na lista.
+     * @return o numero de elementos da lista
+     */
+    public int size() { 
+        return count;
+    }
+
+    /**
+     * Adiciona um elemento ao final da lista.
+     * @param element elemento a ser adicionado ao final da lista
+     */
+    public void add(Integer element) { 
+        if (count == data.length) {
+            setCapacity(data.length * 2);
+        }
+        data[count] = element;
+        count++;
+    }
+
+    /**
+     * Retorna o elemento de uma determinada posicao da lista.
+     * @param index a posicao da lista
+     * @return o elemento da posicao especificada
+     * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
+     */
+    public int get(int index) { // O(1)
+        if ((index < 0) || (index >= count)) {
+            throw new IndexOutOfBoundsException("Index = " + index);
+        }
+        return data[index];
+    }
+    
+    @Override
+    public String toString() { 
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            s.append(data[i]);
+            if (i != (count - 1)) {
+                s.append(",");
+            }
+        }
+        s.append("\n");
+        return s.toString();
+    }
+
+    private void setCapacity(int newCapacity) {
+        if (newCapacity != data.length) {
+            int min = 0;
+            Integer[] newData = new Integer[newCapacity];
+            if (data.length < newCapacity) {
+                min = data.length;
+            } else {
+                min = newCapacity;
+            }
+            for (int i = 0; i < min; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+    }
+
+	
+
+
+  
+}
---
+++ Aula14/app.java
@@ -0,0 +1,27 @@
+package Aula14;
+
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        ListArrayOfInteger lista = new ListArrayOfInteger();
+        lista.add(10);
+        lista.add(20);
+
+        System.out.println(lista);
+
+        System.out.println("Primeiro elemento: " + lista.get(0));
+        
+
+        System.out.println(lista.isEmpty());
+
+        System.out.println(lista.size());
+
+        System.out.println(lista.toString());
+      
+        
+    }
+    
+}
