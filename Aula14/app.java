d52b54dda7e88fcd496693afd2664edbdaca733f
Igor <igorponticelli8@gmail.com>
2023-09-14 11:14:42 -0300

    Aula14.09_2

--- Aula14/app.java
+++ Aula14/app.java
@@ -11,13 +11,15 @@ public class app {
         lista.add(20);
 
         System.out.println(lista);
-
         System.out.println("Primeiro elemento: " + lista.get(0));
-        
+
+        //System.out.println(lista.size());
+        int tam = lista.size();
+        System.out.println("Ultimo elemento da lista: " + lista.get(tam - 1));
 
         System.out.println(lista.isEmpty());
 
-        System.out.println(lista.size());
+        
 
         System.out.println(lista.toString());
       
